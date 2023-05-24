69e8bfbd202d25a2f2488a5c58788944613b91f8
faizakarim <faizakarim999@gmail.com>
2023-05-24 16:17:42 +0500

    add react native image picker

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -12,21 +12,20 @@ import ActionSheet, {
   SheetManager,
 } from 'react-native-actions-sheet';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 interface Iprops {
   ref: any;
 }
 interface BottomSheetProps {
-  children: ReactNode;
   snapPoints?: 75;
   id: string;
   style?: StyleProp<ViewStyle>;
+  children: ReactNode;
 }
 
 export const BottomSheet = forwardRef<any, BottomSheetProps>((Props, ref) => {
   const actionSheetRef = useRef<ActionSheetRef>(null);
-  const {snapPoints = 75, children, id, style} = Props || {};
+  const {snapPoints = 75, id, children} = Props || {};
 
   useImperativeHandle(ref, () => ({
     show() {
@@ -36,31 +35,13 @@ export const BottomSheet = forwardRef<any, BottomSheetProps>((Props, ref) => {
       SheetManager.hide(id);
     },
   }));
-  const Arr = [
-    {
-      heading: 'Add from gellary.',
-      IconName: 'image-size-select-actual',
-      color: '#242B2E',
-    },
-    {heading: 'Add from facbook.', IconName: 'facebook', color: '#3944F7'},
-    {heading: 'Add from instagram.', IconName: 'instagram', color: '#6A1B4D'},
-    {
-      heading: 'Add from  googlePhotos.',
-      IconName: 'google-drive',
-      color: '#2827CC',
-    },
-  ];
+
   return (
     <ActionSheet
       ref={actionSheetRef}
       id={id}
       containerStyle={styles.actionSheet}>
-      {Arr.map(item => (
-        <TouchableOpacity style={styles.bottomSheetView}>
-          <Icon name={item.IconName} size={15} color={item.color} />
-          <Text style={styles.text}>{item.heading}</Text>
-        </TouchableOpacity>
-      ))}
+      <View>{children}</View>
     </ActionSheet>
   );
 });
@@ -69,16 +50,5 @@ const styles = StyleSheet.create({
   actionSheet: {
     paddingHorizontal: 16,
     paddingTop: 15,
-  },
-  bottomSheetView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 4,
-  },
-  text: {
-    marginLeft: 20,
-    fontSize: 11,
-    fontWeight: 'bold',
-    width: '100%',
   },
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,29 +1,79 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import React, {useRef} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from 'react-native';
+import React, {useRef, useState} from 'react';
 import {Header} from '../components/Header';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {BottomSheet} from '../components/BottomSheet';
-import {Modalize} from 'react-native-modalize';
-
+import {CameraOptions, launchImageLibrary} from 'react-native-image-picker';
 const HomeScreen = () => {
   const actionSheetRef = useRef();
+  const [image, setImage] = useState('');
+
+  const [filePath, setFilePath] = useState({});
+  const Arr = [
+    {
+      heading: 'Add from Gallery.',
+      IconName: 'image-size-select-actual',
+      color: '#242B2E',
+    },
+    {heading: 'Add from Facbook.', IconName: 'facebook', color: '#3944F7'},
+    {heading: 'Add from Instagram.', IconName: 'insatagram', color: '#6A1B4D'},
+    {
+      heading: 'Add from  Google Photos.',
+      IconName: 'google-drive',
+      color: '#2827CC',
+    },
+  ];
+  const chooseFile = async () => {
+    let options: CameraOptions = {
+      mediaType: 'photo',
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+    };
+    let gallery = await launchImageLibrary(options);
+    const imagePath = gallery?.assets?.[0]?.uri ?? '';
+    setImage(imagePath);
+  };
+
   return (
-    <View style={style.container}>
+    <View style={styles.container}>
       <View>
         <Header />
       </View>
-      <View style={style.memoryView}>
+      <View>
+        {image && (
+          <Image source={{uri: image}} style={{width: 200, height: 200}} />
+        )}
+      </View>
+      <View style={styles.memoryView}>
         <Text>Add your memory </Text>
-        <TouchableOpacity onPress={() => actionSheetRef.current?.show()}>
+        <TouchableOpacity
+          onPress={() => {
+            actionSheetRef?.current?.show();
+          }}>
           <Icon name="add-a-photo" size={25} />
         </TouchableOpacity>
       </View>
-      <BottomSheet ref={actionSheetRef} id={'1'} />
+      <BottomSheet ref={actionSheetRef} id={'1'}>
+        {Arr.map(item => (
+          <TouchableOpacity style={styles.bottomSheetView} onPress={chooseFile}>
+            <Icon name={item.IconName} size={15} color={item.color} />
+            <Text style={styles.text}>{item.heading}</Text>
+          </TouchableOpacity>
+        ))}
+      </BottomSheet>
     </View>
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     backgroundColor: '#CAD5E2',
     flex: 1,
@@ -41,6 +91,17 @@ const style = StyleSheet.create({
     bottom: 10,
     alignSelf: 'center',
     width: '95%',
+  },
+  bottomSheetView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 4,
+  },
+  text: {
+    marginLeft: 20,
+    fontSize: 11,
+    fontWeight: 'bold',
+    width: '100%',
   },
 });
 export default HomeScreen;
